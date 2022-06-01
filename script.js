302d8991d8ce829aba0eb181e23eea10805f9ba3
Sebastian Linnet <sebastian.linnet@gmail.com>
2022-06-01 09:34:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function fullScreen(element) {
+    if (element.requestFullScreen) {
+        element.requestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    }
+    //link = document.getElementById("container");
+    //link.removeAttribute("onclick");
+    //link.setAttribute("onclick", "cancelFullScreen()");
+}
+
+window.onload = function() {
+    imgs = document.getElementById('slideshow').children;
+    picInterval = 15000;
+    currentPic = 0;
+    //imgs[currentPic].style.webkitAnimation = 'fadeImg '+interval+'ms';
+    imgs[currentPic].style.animation = 'fadeImg '+picInterval+'ms';
+
+    setInterval(function(){
+        imgs[currentPic].removeAttribute('style');
+        if ( currentPic == imgs.length - 1) {
+            currentPic = 0; 
+        } else { 
+            currentPic++; 
+        }
+        //imgs[currentPic].style.webkitAnimation = 'fadeImg '+interval+'ms';
+        imgs[currentPic].style.animation = 'fadeImg '+picInterval+'ms';
+    }, picInterval);
+}
